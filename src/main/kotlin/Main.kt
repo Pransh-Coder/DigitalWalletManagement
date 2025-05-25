8d1958c078bcbe0f22e595682526a55e9be196f0
Pransh_Rastogi <rastogipransh4@gmail.com>
2025-05-25 13:53:16 +0530

    added pojos that I thought of before starting to code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,40 @@
 package org.example
 
+/* POJOs / DATA class
+
+User
+--userId
+--name
+
+Account
+--accountNum
+--user: User
+--currentBalance
+--Set<Transactions>
+
+Transaction
+--transactionId
+--fromName
+--toName
+--amount (sent/received)
+--timeStampWithDate
+----------------------------------
+Create Wallet —  This option should create a wallet for the user.
+walletId
+name
+initialAmount
+qrCodeData
+timeStamp
+
+Transfer Amount
+transactionId
+name (sender/receiver info)
+accountNum
+amount (sent/received)
+timeStamp
+*/
+
 fun main() {
-    //println("Hello World!")
 
     val walletRepository = WalletRepository()
 
